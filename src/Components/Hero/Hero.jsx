f88cdeb0c1b85b29f90c36cdd738326bfa3cb92f
esnoko <elvisnoko18@gmail.com>
2025-03-25 22:37:05 +0200

    initial commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,13 +8,9 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm NokoDev</h1>
+        <h1 className={styles.title}>Hi, I'm Kgomo EN</h1>
         <p className={styles.description}>
-          I'm a junior software developer with 2 years of experience using React
-          and NodeJS. I specialize in creating dynamic and responsive web
-          applications. I am passionate about learning new technologies and
-          improving my skills. Reach out if you'd like to learn more about my
-          work or discuss potential opportunities!
+          
         </p>
         <a href="mailto:elvisnoko18@email.com" className={styles.contactBtn}>
           Contact Me

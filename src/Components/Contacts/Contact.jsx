a58a493ae53f5538e35780d379ddc8bac9125f44
esnoko <elvisnoko18@gmail.com>
2025-03-31 01:12:09 +0200

    Refactor Contact component and add footer

--- src/Components/Contacts/Contact.jsx
+++ src/Components/Contacts/Contact.jsx
@@ -6,28 +6,36 @@ import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
     return (
-        <section id="contact" className={styles.container}>
-            <div className={styles.text}>
-                <h2>Contact</h2>
-                <p>Feel free to reach out!</p>
-            </div>
-            <ul className={styles.links}>
-                <li className={styles.link}>
-                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-                    <a href="mailto:elvisnoko18@email.com">elvisnoko18@email.com</a>
-                </li>
-                <li className={styles.link}>
-                    <img
-                        src={getImageUrl("contact/linkedinIcon.png")}
-                        alt="LinkedIn icon"
-                    />
-                    <a href="https://linkedin.com/in/nokodev/">https://linkedin.com/in/nokodev/</a>
-                </li>
-                <li className={styles.link}>
-                    <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-                    <a href="https://github.com/esnoko">github.com/esnoko</a>
-                </li>
-            </ul>
-        </section>
+        <>
+            <section id="contact" className={styles.container}>
+                <div className={styles.text}>
+                    <h2>Contact</h2>
+                    <p>Feel free to reach out!</p>
+                </div>
+                <ul className={styles.links}>
+                    <li className={styles.link}>
+                        <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                        <a href="mailto:elvisnoko18@email.com">elvisnoko18@email.com</a>
+                    </li>
+                    <li className={styles.link}>
+                        <img
+                            src={getImageUrl("contact/linkedinIcon.png")}
+                            alt="LinkedIn icon"
+                        />
+                        <a href="https://linkedin.com/in/nokodev/">https://linkedin.com/in/nokodev/</a>
+                    </li>
+                    <li className={styles.link}>
+                        <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+                        <a href="https://github.com/esnoko">github.com/esnoko</a>
+                    </li>
+                </ul>
+            </section>
+
+            <footer className={styles.footer}>
+                <div className={styles.footerContent}>
+                    <p>© {new Date().getFullYear()} Your Name. All rights reserved.</p>
+                </div>
+            </footer>
+        </>
     );
 };
